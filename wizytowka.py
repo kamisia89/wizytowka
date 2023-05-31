e899a3cd8101d061c5824ac191f31d1beff2b1aa
Kamila Kolopoleska <Kamila.eska89@gmail.com>
2023-05-31 05:43:01 +0100

    more details added

--- wizytowka.py
+++ wizytowka.py
@@ -18,21 +18,35 @@ fake.add_provider(travel_professions_provider)
 
 
 class Card:
-    def __init__(self, name, surname, company, position, email):
+    def __init__(self, name, surname, email, travel_profession, company):
         self.name = name
         self.surname = surname
-        self.company = company
-        self.position = position
         self.email = email
+        self.profession = travel_profession
+        self.company = company
 
 
-# Derek_Coon = Card(
-# name="Derek",
-# surname="Coon",
-# company="K&G Distributors",
-# position="Network architect",
-# email="DerekJCoon@dayrep.com",
-# )
-# print(Card)
-for _ in range(5):
-    print(fake.name(), fake.email(), fake.travel_profession(), fake.company())
+# for _ in range(5):
+# print(fake.name(), fake.email(), fake.travel_profession(), fake.company())
+Meghan_Martin = Card(
+    "Meghan", "Martin", "alexander29@example.org", "tour manager", "Gallegos-Williams"
+)
+Todd_Fuentes = Card(
+    "Todd",
+    "Fuentes",
+    "raymond26@example.com",
+    "tourist guide",
+    "Bowen Sanford and Huerta",
+)
+Diana_Cummings = Card(
+    "Diana", "Martn", "kathryn73@example.org", "travel agent", "Hancock and Sons"
+)
+Tiffany_Camacho = Card(
+    "Tiffany", "Camacho", "frichard@example.com", "tourist guide", "Moore Group"
+)
+Cody_Fletcher = Card(
+    "Cody", "Fletcher", "amberdunn@example.com", "hotel manager", "Russell Inc"
+)
+card = Card()
+# by_name = sorted(Card, key=lambda Card: Card.name)
+print(card())
