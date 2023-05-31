db8ed67891b06bcacef3aa925d3598e0e2026eb7
Kamila Kolopoleska <Kamila.eska89@gmail.com>
2023-05-31 18:58:40 +0100

    new ideas, new version

--- wizytowka.py
+++ wizytowka.py
@@ -17,36 +17,107 @@ fake = Faker()
 fake.add_provider(travel_professions_provider)
 
 
-class Card:
-    def __init__(self, name, surname, email, travel_profession, company):
+class BaseContact:
+    def __init__(self, name, surname, phone_number, email):
         self.name = name
         self.surname = surname
+        self.phone_number = phone_number
         self.email = email
-        self.profession = travel_profession
+        self.name = 0
+
+
+class BusinessContact(BaseContact):
+    def __init__(self, occupation, company, business_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.role = role
         self.company = company
+        self.business_phone = business_phone
 
 
-# for _ in range(5):
+def __repr__(self):
+    return f"{self.business_phone} {self.name} {self.surname}"
+
+
+def contact(self):
+    return f"Wybieram numer {self.business_phone}{self.phone_number} i dzwonię do {self.name}{self.surname}”."
+
+
+# basic = BaseContact()
+
+# TO PSUJE CALY KOD => card = Card()
+
+# def contact(self):
+#   return f"{self.name}{self.surname}
+
+
+# TEN FKER JEST SUPER WYMYSLIL MI IMIONA ITD. ALE MUSIALAM I TAK WSZYSTKO SKOPIOWAC BO JAK MIALAM WSZYSTKO WYMIENIONE W KUPIE NIE WIEDZILAM JAK TO POGRUPOWACI DUPA
+def create_contacts(num_contacts):
+    contact_list = []
+
+
+for i in range(1, num_contacts):
+    contact = {}
+    contact["name"] = fake.name()
+    contact["surname"] = fake.surname()
+    contact["phone_number"] = fake.phone()
+    contact["email"] = fake.email()
+    contact["role"] = fake.random_element(
+        elements=(
+            "Hotel Manager",
+            "Travel Agent",
+            "Tour Operator",
+            "Tourist Guide",
+            "Tour Manager",
+            "Sailing Instructor",
+        )
+    )
+    contact["company"] = fake.company()
+    contact["business_number"] = fake.phone()
+    contact_list.append(contact)
+return pd.DataFrame(contact_list)
+
 # print(fake.name(), fake.email(), fake.travel_profession(), fake.company())
-Meghan_Martin = Card(
-    "Meghan", "Martin", "alexander29@example.org", "tour manager", "Gallegos-Williams"
-)
-Todd_Fuentes = Card(
-    "Todd",
-    "Fuentes",
-    "raymond26@example.com",
-    "tourist guide",
-    "Bowen Sanford and Huerta",
-)
-Diana_Cummings = Card(
-    "Diana", "Martn", "kathryn73@example.org", "travel agent", "Hancock and Sons"
-)
-Tiffany_Camacho = Card(
-    "Tiffany", "Camacho", "frichard@example.com", "tourist guide", "Moore Group"
-)
-Cody_Fletcher = Card(
-    "Cody", "Fletcher", "amberdunn@example.com", "hotel manager", "Russell Inc"
-)
-card = Card()
-# by_name = sorted(Card, key=lambda Card: Card.name)
-print(card())
+
+
+# people = [Meghan_Martin, Todd_Fuentes, Diana_Cummings, Tiffany_Camacho, Cody_Fletcher]
+
+
+# def __str__(self):
+# return f"{self.name} {self.surname} {self.email} {self.travel_profession} {self.company} "
+
+
+# def __repr__(self):
+# return f"Card(name={self.name} surname={self.surname}, email={self.email}, travel_profession={self.travel_profession}, company={self.company})"
+
+
+# print(people)
+
+# by_name = sorted(people, key=lambda Card: Card.name)
+# lambda Card: Card.name
+# print(by_name)
+# by_surname = sorted(people, key=lambda Card: Card.surname)
+# lambda Card: Card.surname
+# print(by_surname)
+# by_email = sorted(people, key=lambda Card: Card.email)
+# lambda Card: Card.email
+# print(by_email)
+
+
+# def contact(self):
+# return (
+# f"Wybieram adres e-mail  {self.email} i piszę do {self.name} {self.surname}”."
+# )
+# @property
+# def name_sum(self):
+#   return self._name_sum
+
+# @name_sum.setter
+#   def name_sum(self, a):
+#      a = (len(self.name) + len(self.surname))
+#     self._name_sum = a
+
+
+# print(Card())
+# print(Cody_Fletcher.contact)
+# def contact(self):
+# return f"Wybieram numer {self.company_phone}{self.phone} i dzwonię do {self.name}{self.surname}”."
